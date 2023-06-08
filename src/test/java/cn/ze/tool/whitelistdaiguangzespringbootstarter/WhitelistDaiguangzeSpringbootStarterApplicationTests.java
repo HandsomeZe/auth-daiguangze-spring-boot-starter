6ca565de38d62fde6b456e73316f6ecbaa3bab33
daiguangze <1914088101@qq.com>
2023-06-08 18:31:27 +0800

    init

---
+++ src/test/java/cn/ze/tool/whitelistdaiguangzespringbootstarter/WhitelistDaiguangzeSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ze.tool.whitelistdaiguangzespringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitelistDaiguangzeSpringbootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
