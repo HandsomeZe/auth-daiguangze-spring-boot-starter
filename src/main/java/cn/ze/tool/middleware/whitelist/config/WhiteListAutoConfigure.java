4d62eb91d8d13732f34132b0bc3bc0e227b235bc
daiguangze <1914088101@qq.com>
2023-06-08 20:45:53 +0800

    feat: 完成简单的白名单, springboot-starter 形式

--- src/main/java/cn/ze/tool/middleware/whitelist/config/WhiteListAutoConfigure.java
+++ src/main/java/cn/ze/tool/middleware/whitelist/config/WhiteListAutoConfigure.java
@@ -1,4 +1,20 @@
 package cn.ze.tool.middleware.whitelist.config;
 
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(WhiteListProperties.class)
+@EnableConfigurationProperties(WhiteListProperties.class)
 public class WhiteListAutoConfigure {
+
+    @Bean("whiteListConfig")
+    @ConditionalOnMissingBean
+    public String whilteListConfig(WhiteListProperties properties){
+        return properties.getUsers();
+    }
 }
--- src/main/java/cn/ze/tool/middleware/whitelist/config/WhiteListProperties.java
+++ src/main/java/cn/ze/tool/middleware/whitelist/config/WhiteListProperties.java
@@ -1,4 +1,15 @@
 package cn.ze.tool.middleware.whitelist.config;
 
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("cn.ze.tool.whitelist")
 public class WhiteListProperties {
+    private String users;
+
+    public String getUsers(){return users;}
+
+    public void setUsers(String users){
+        this.users = users;
+    }
 }
